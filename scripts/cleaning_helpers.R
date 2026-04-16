6883359ac6b45d1d97540a8ae1c4ed2a09fcc664
Kristina Stoianova <238165910+Kristina-Stoianova@users.noreply.github.com>
2026-04-16 21:47:32 +0100

    minor changes to functions

--- scripts/cleaning_helpers.R
+++ scripts/cleaning_helpers.R
@@ -7,20 +7,56 @@ library(stringr)
 
 #standardize missing values to N/A
 standardise_missing_values <- function(df) {
-  missing_vals <- c("", "na", "n/a", "missing", "_", "not on bbs", "incomplete chart", "incomplete data", "see comments", "-", "tftc")
+  missing_vals <- c(
+    "", "na", "n/a", "missing", "_", "not on bbs",
+    "incomplete chart", "incomplete data", "see comments",
+    "-", "tftc", ".", "null", "none", "pending", "not specified"
+  )
   df %>%
     dplyr::mutate(
       across(
-      where(is.character),
-      ~ {
-        x <- stringr::str_squish(.x) #remove trailing/leading empty spaces
-        x_lower <- stringr::str_to_lower(x) #
-        x[x_lower %in% missing_vals] <- NA
-        x
-      }
+        where(is.character),
+        ~ {
+          x <- stringr::str_squish(.x)  #trim spaces
+          x_lower <- stringr::str_to_lower(x)
+          x[!is.na(x_lower) & x_lower %in% missing_vals] <- NA
+          x
+        }
+      )
     )
+}
+
+#clean funding
+clean_funding <- function(x) {
+  x %>%
+    stringr::str_squish() %>%
+    stringr::str_remove("^\\d+\\s*[-–]\\s*")
+}
+
+#clean protocol type
+clean_protocol_type <- function(x) {
+  x_clean <- x %>%
+    stringr::str_squish() %>%
+    stringr::str_to_lower() %>%
+    stringr::str_replace_all("[^a-z0-9 ]", " ")  #Removes + symbols
+  
+  dplyr::case_when(
+    stringr::str_detect(x_clean, "egg donor|donor") ~ "Egg_donor",
+    stringr::str_detect(x_clean, "fertility preservation") ~ "Fertility_preservation",
+    stringr::str_detect(x_clean, "long agonist") ~ "Long_agonist",
+    stringr::str_detect(x_clean, "short") ~ "Short_antagonist",
+    TRUE ~ "Other"
   )
 }
+
+#clean doses in protocol
+extract_fsh_dose <- function(x) {
+  as.numeric(stringr::str_extract(x, "(?<=rfsh\\s)\\d+|(?<=fsh\\s)\\d+"))
+}
+extract_hmg_dose <- function(x) {
+  as.numeric(stringr::str_extract(x, "(?<=hmg\\s)\\d+"))
+}
+
 
 #clean categorical variables/strings 
 clean_categorical <- function(x){
